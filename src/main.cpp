90c771411a062c578ea1f39eef174b4ec7f4eb0c
Min Liu <wzygzlm@gmail.com>
2020-07-21 12:24:36 +0200

    Port name changed.

--- src/main.cpp
+++ src/main.cpp
@@ -8,30 +8,30 @@
 void eventStreamDuplicate(ap_uint<32> config,
 		hls::stream< ap_uint<16> > &xStreamIn, hls::stream< ap_uint<16> > &yStreamIn,
 		hls::stream< ap_uint<64> > &tsStreamIn, hls::stream< ap_uint<1> > &polStreamIn,
-		hls::stream< ap_uint<16> > &cornerStreamIn,
+		hls::stream< ap_uint<16> > &custDataStreamIn,
 		hls::stream< ap_uint<16> > &xStreamOut0, hls::stream< ap_uint<16> > &yStreamOut0,
 		hls::stream< ap_uint<64> > &tsStreamOut0, hls::stream< ap_uint<1> > &polStreamOut0,
-		hls::stream< ap_uint<16> > &cornerStreamOut0,
+		hls::stream< ap_uint<16> > &custDataStreamOut0,
 		hls::stream< ap_uint<16> > &xStreamOut1, hls::stream< ap_uint<16> > &yStreamOut1,
 		hls::stream< ap_uint<64> > &tsStreamOut1, hls::stream< ap_uint<1> > &polStreamOut1,
-		hls::stream< ap_uint<16> > &cornerStreamOut1)
+		hls::stream< ap_uint<16> > &custDataStreamOut1)
 {
 #pragma HLS PIPELINE
 #pragma HLS INTERFACE s_axilite port=config bundle=config
 
-#pragma HLS INTERFACE axis register both port=cornerStreamOut1
+#pragma HLS INTERFACE axis register both port=custDataStreamOut1
 #pragma HLS INTERFACE axis register both port=polStreamOut1
 #pragma HLS INTERFACE axis register both port=tsStreamOut1
 #pragma HLS INTERFACE axis register both port=yStreamOut1
 #pragma HLS INTERFACE axis register both port=xStreamOut1
 
-#pragma HLS INTERFACE axis register both port=cornerStreamOut0
+#pragma HLS INTERFACE axis register both port=custDataStreamOut0
 #pragma HLS INTERFACE axis register both port=polStreamOut0
 #pragma HLS INTERFACE axis register both port=tsStreamOut0
 #pragma HLS INTERFACE axis register both port=yStreamOut0
 #pragma HLS INTERFACE axis register both port=xStreamOut0
 
-#pragma HLS INTERFACE axis register both port=cornerStreamIn
+#pragma HLS INTERFACE axis register both port=custDataStreamIn
 #pragma HLS INTERFACE axis register both port=polStreamIn
 #pragma HLS INTERFACE axis register both port=tsStreamIn
 #pragma HLS INTERFACE axis register both port=yStreamIn
@@ -40,13 +40,13 @@ void eventStreamDuplicate(ap_uint<32> config,
 	ap_uint<16> x, y;
 	ap_uint<1> pol;
 	ap_uint<64> ts;
-	ap_uint<16> corner;
+	ap_uint<16> custData;
 
 	xStreamIn >> x;
 	yStreamIn >> y;
 	polStreamIn >> pol;
 	tsStreamIn >> ts;
-	cornerStreamIn >> corner;
+	custDataStreamIn >> custData;
 
 	if(config.range(1, 0) == 0)
 	{
@@ -54,7 +54,7 @@ void eventStreamDuplicate(ap_uint<32> config,
 		yStreamOut0 << y;
 		polStreamOut0 << pol;
 		tsStreamOut0 << ts;
-		cornerStreamOut0 << corner;
+		custDataStreamOut0 << custData;
 	}
 	else if(config.range(1, 0) == 1)
 	{
@@ -62,7 +62,7 @@ void eventStreamDuplicate(ap_uint<32> config,
 		yStreamOut1 << y;
 		polStreamOut1 << pol;
 		tsStreamOut1 << ts;
-		cornerStreamOut1 << corner;
+		custDataStreamOut1 << custData;
 	}
 	else if(config.range(1, 0) == 2)
 	{
@@ -70,13 +70,13 @@ void eventStreamDuplicate(ap_uint<32> config,
 		yStreamOut0 << y;
 		polStreamOut0 << pol;
 		tsStreamOut0 << ts;
-		cornerStreamOut0 << corner;
+		custDataStreamOut0 << custData;
 
 		xStreamOut1 << x;
 		yStreamOut1 << y;
 		polStreamOut1 << pol;
 		tsStreamOut1 << ts;
-		cornerStreamOut1 << corner;
+		custDataStreamOut1 << custData;
 	}
 	else
 	{
